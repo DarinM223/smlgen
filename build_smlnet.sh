879f1c19a8507570d73a745c32f10dd2b24f05e1
d <darinm223@gmail.com>
2025-08-22 10:37:31 -0700

    Modify README.md and check building on all compilers

--- build_smlnet.sh
+++ build_smlnet.sh
@@ -1,2 +1,2 @@
-echo "@$SMLNETPATH/lib/smlnj-lib/Util/Util" > Deps.smlnet
-echo "@$SMLNETPATH/lib/posix/Posix" >> Deps.smlnet
+echo "@$SMLNETPATH/lib/smlnj-lib/Util/Util" > smldotnet/Deps.smlnet
+echo "@$SMLNETPATH/lib/posix/Posix" >> smldotnet/Deps.smlnet
